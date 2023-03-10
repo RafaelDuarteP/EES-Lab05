c8e1e9a58f6485f2bd0bb9e3bfaeb9d3077f73c7
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-03-10 11:58:14 -0300

    Adicionando comentarios e uma linha de instrução

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
+/***
+ * 
+ * @author rafael
+ *
+ */
 public class Main {
-
-	public static void main(String[] args) {
+	
+	/***
+	 * Método para imprimir o nome do aluno e o nome da disciplina
+	 * 
+	 * @param args argumentos da jvm
+	 */
+	public static void main(String[] args) 
+	{
+		//Imprime o nome do aluno
 		System.out.println("Rafael Duarte Pereira");
+		//Imprime o nome da disciplina
 		System.out.println("Engenharia Economica para Software");
+		//Imprime o resultado de 2+2
+		System.out.println(2+2);
 
 	}
 
